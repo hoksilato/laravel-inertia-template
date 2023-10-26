15cb046f9165e08a778e461021b6fd78fb00b6f6
Cang <hoksilato176@gmail.com>
2023-10-26 11:36:06 +0700

    feat: install shadcn-ui

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/Components/ui/button'
+</script>
+
 <template>
     <h1 class="italic">Hello World!</h1>
-</template>
 
-<script>
-export default {};
-</script>
+    <div>
+        <Button>Click me</Button>
+    </div>
+</template>
