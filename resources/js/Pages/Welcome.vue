73a9a0c0fd8834680b00272cc09b133d0a1568cb
Cang Ta <hoksilato176@gmail.com>
2023-11-15 17:28:42 +0700

    feat: set up typescript, tailwindcss & shadcn (#1)
    * chore: install typescript
    
    * feat: install tailwindcss
    
    * feat: install shadcn-ui
    
    * doc: update README

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,7 +1,11 @@
-<template>
-    <h1>Hello World!</h1>
-</template>
-
-<script>
-export default {};
+<script setup lang="ts">
+import { Button } from '@/Components/ui/button'
 </script>
+
+<template>
+    <h1 class="italic">Hello World!</h1>
+
+    <div>
+        <Button>Click me</Button>
+    </div>
+</template>
